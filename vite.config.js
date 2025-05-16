241f1563404ce7bed2af8d5d43747bd2e2d923b8
SupriyaSharma-git <sharmasupriya0986@gmail.com>
2025-05-17 01:02:32 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/Tic-Tac-Toe-Gamee"
+  base: "/Tic-Tac-Toe-Game"
 });
